ed7716f63a4cb8b4a80c3714d76f1639c7f13dd2
Desu Sai Venkat <sai-pt3593@csez.zohocorpin.com>
2020-06-01 10:22:49 +0530

    returning url

--- api.py
+++ api.py
@@ -1,18 +1,8 @@
 import requests
+import urllib
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify, request 
-payload = ""
-headers = {
-        'accept-language': "en-US,en;q=0.9",
-        'accept-encoding': "gzip, deflate, br",
-        'accept': "*/*",
-        'x-api-source': "pc",
-        'user-agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-        'if-none-match-': "55b03-e2b4b3f247507f7c1a18fda4a09f1340",
-        'x-requested-with': "XMLHttpRequest",
-        'connection': "keep-alive",
-        'host': "runningstatus.in"
-        }
+
 app = Flask(__name__) 
 @app.route('/', methods = ['GET', 'POST']) 
 def home(): 
@@ -25,8 +15,9 @@ def home():
 def disp(trainnum): 
   date = '20200531'
   url = 'https://runningstatus.in/status/'+trainnum+'-on-20200531'
-  res = requests.get(url,data=payload, headers=headers)
-  return res.text
+  print(url)
+  
+  return url
 
   
 if __name__ == '__main__': 
