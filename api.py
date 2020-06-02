bb2894ca7f859499f4e296348075f1c458c7b122
Desu Sai Venkat <sai-pt3593@csez.zohocorpin.com>
2020-06-02 11:56:23 +0530

    added headers

--- api.py
+++ api.py
@@ -15,7 +15,8 @@ def home():
 def disp(trainnum): 
   date = '20200601'
   url = 'https://runningstatus.in/status/'+trainnum+'-on-'+date
-  res = requests.get(url)
+  headers = {'Host': 'runningstatus.in','Connection': 'keep-alive','Cache-Control': 'max-age=0','Upgrade-Insecure-Requests': 1,'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','Sec-Fetch-Site': 'cross-site','Sec-Fetch-Mode': 'navigate','Sec-Fetch-Dest': 'document','Accept-Encoding': 'gzip, deflate, br','Accept-Language': 'en-US,en;q=0.9,hi;q=0.8'}
+  res = requests.get(url,headers=headers)
   if not res:
     return str(res.status_code)
   soup = BeautifulSoup(res.text,'html.parser')
