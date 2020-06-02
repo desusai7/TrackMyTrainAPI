3f4873d4040e3443cbd5abfee70d8c288b0e56e9
Desu Sai Venkat <sai-pt3593@csez.zohocorpin.com>
2020-06-02 23:05:49 +0530

    added status code

--- api.py
+++ api.py
@@ -15,7 +15,7 @@ def check():
    url = "https://runningstatus.in/status/02566"
    res  = requests.get(url,headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    })
+    },timeout=10)
    return str(res.status_code)
 @app.route('/status/<trainnum>', methods = ['GET']) 
 def disp(trainnum): 
