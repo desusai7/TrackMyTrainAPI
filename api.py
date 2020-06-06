90702fc37229d15abb2453cad3718b4cdb598557
Desu Sai Venkat <sai-pt3593@csez.zohocorpin.com>
2020-06-06 12:33:16 +0530

    working on local check

--- api.py
+++ api.py
@@ -17,8 +17,15 @@ def check():
    finder = re.findall(r'currentStnName = .*;', page_html.decode("utf-8"))
    midvalue = finder[0].split("currentStnName = \"")[1]
    stationname = midvalue.split("\";")[0]
-   print(stationname)
-   return stationname
+   stationname = stationname.strip()
+   soup = BeautifulSoup(page_html,'html.parser')
+   divs = soup.find_all("div", class_="row status-summary")[0]
+   result = "".join(divs.strings)
+   resultlist = result.splitlines()
+   resultlist[1] = resultlist[1]+stationname
+   print(resultlist[1])
+   print(resultlist[2])
+   return resultlist[1]+" "+resultlist[2]
 
 @app.route('/status/<trainnum>', methods = ['GET']) 
 def disp(trainnum): 
